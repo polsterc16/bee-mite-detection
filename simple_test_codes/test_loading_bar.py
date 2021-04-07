bcc575ed503c4c3af47d7e4ad456ad22248acb9b
chris <christoph.polster@edu.fh-joanneum.at>
2021-04-07 14:50:21 +0200

    little test with TQDM for potential progress indicator

---
+++ simple_test_codes/test_loading_bar.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  7 12:54:06 2021
+
+@author: Admin
+"""
+
+#Importing tqdm function of tqdm module 
+from tqdm import tqdm  
+from time import sleep 
+for i in tqdm(range(200)):  
+# Waiting for 0.01 sec before next execution 
+   sleep(.01) 
+   pass
+   
+#%%
+from tqdm import tqdm 
+for i in tqdm(range(int(5000000)), desc="Progress"): 
+   pass
+   
+#%%
+for i in tqdm(range(0, 100), disable=True): 
+    sleep(.01) 
+print("Done") 
+#%%
+
