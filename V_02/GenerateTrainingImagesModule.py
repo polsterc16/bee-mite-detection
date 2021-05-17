6957522e9bc7a2ced972983078502fff7b3856e6
chris <christoph.polster@edu.fh-joanneum.at>
2021-05-17 14:25:13 +0200

    added new file for the prepareation/generation of training images

---
+++ V_02/GenerateTrainingImagesModule.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 14:22:43 2021
+
+@author: Admin
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import os
+import random
+
+from tqdm import tqdm
+from ast import literal_eval
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %% 
+
+if __name__== "__main__":
+    print("## Calling main function.)\n")
+    
+    print("cv2.version = {}".format(cv2.__version__))
+    print("numpy.version = {}".format(np.__version__))
+    print("matplotlib.version = {}".format(mpl.__version__))
+    print("pandas.version = {}".format(pd.__version__))
+    print()
+    
+    
+    # Window Cleanup
+    cv2.destroyAllWindows()
+    plt.close('all')
+    
+    TEST = 1
+    
+    # %%
+    if TEST == 1:
+        
+        
+        
+        
+        
+        
