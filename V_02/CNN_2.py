97f295dabae0032a604e2a4138a4b09310da1382
chris <christoph.polster@edu.fh-joanneum.at>
2021-05-19 10:47:13 +0200

    adjusted for testing hyperparameters. Realized pretty quickly, that the whole thing is just about brute forcing all the combinations over a few epochs to have a reasonable representation of the result... That requires time and processing power - Things I currently do not have...

--- V_02/CNN_2.py
+++ V_02/CNN_2.py
@@ -41,14 +41,95 @@ print("pandas.version = {}".format(pd.__version__))
 print("tensorflow.version = {}".format(tf.__version__))
 print()
 
+
+
+
+#%%
+
+
+# https://www.tensorflow.org/tensorboard/hyperparameter_tuning_with_hparams
+HP_NUM_UNITS =  hp.HParam('num_units', hp.Discrete([128, 256, 512]))
+HP_NUM_FILTERS =    hp.HParam('filters', hp.Discrete([8, 16, 32]))
+# HP_KERNEL_SIZE =    hp.HParam('kernel_size', hp.Discrete([3, 5]))
+# HP_STRIDES =    hp.HParam('strides', hp.Discrete([1,2,3]))
+# HP_DROPOUT =    hp.HParam('dropout', hp.RealInterval(0.1, 0.5))
+# HP_DROPOUT =    hp.HParam('dropout', hp.Discrete( [0.1, 0.2, 0.3, 0.4, 0.5] ))
+# HP_POOL_SIZE =  hp.HParam('pool_size', hp.Discrete([2, 4]))
+
+# HP_OPTIMIZER =  hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
+
+METRIC_ACCURACY = 'accuracy'
+
+
+
+def train_test_model(dg_train, dg_test, input_shape, output_len, hparams, epochs:int=1):
+    
+    model = keras.models.Sequential()
+    
+    
+    model.add(keras.layers.Conv2D(
+        filters =       hparams[HP_NUM_FILTERS],
+        kernel_size =   (3,3),
+        # strides =       (hparams[HP_STRIDES],hparams[HP_STRIDES]),
+        input_shape = temp_img.shape, activation = 'relu' ))
+    
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D( pool_size = ( 2,2 ) ))
+    model.add(keras.layers.Dropout( 0.3 ))
+    
+    
+    model.add(keras.layers.Conv2D(
+        filters =       2*hparams[HP_NUM_FILTERS],
+        kernel_size =   (3,3),
+        # strides =       (hparams[HP_STRIDES],hparams[HP_STRIDES]),
+        activation = 'relu' ))
+    
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.MaxPool2D( pool_size = ( 2,2 ) ))
+    model.add(keras.layers.Dropout( 0.4 ))
+    
+    # model.add(layers.Conv2D(filters = 64, kernel_size = (3,3), activation = 'relu'))
+    # model.add(tf.keras.layers.BatchNormalization())
+    # model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2))) # 16*16
+    # model.add(tf.keras.layers.Dropout(0.5))
+    
+    model.add(keras.layers.Flatten())
+    
+    model.add(keras.layers.Dense(units = hparams[HP_NUM_UNITS], activation = 'relu')) # hidden layer 1
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dropout( 0.5 ))
+    
+    model.add(keras.layers.Dense(units = hparams[HP_NUM_UNITS], activation = 'relu')) # hidden layer 2
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dropout( 0.5 ))
+    
+    model.add(keras.layers.Dense(units=2, activation='sigmoid'))   # output layer
+    
+    model.compile(optimizer = "adam", loss = 'binary_crossentropy', metrics = ['accuracy'])
+    # early_stop = keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 5)
+
+    
+    model.fit(dg_train,
+              # steps_per_epoch=64,
+              epochs=epochs, verbose=1) 
+    _, accuracy = model.evaluate(dg_test)
+    return accuracy
+
+def run(run_dir, dg_train, dg_test, input_shape, output_len, hparams, epochs:int=1):
+    with tf.summary.create_file_writer(run_dir).as_default():
+        hp.hparams(hparams)  # record the values used in this trial
+        
+        accuracy = train_test_model(dg_train, dg_test, input_shape, output_len, hparams, epochs)
+        
+        tf.summary.scalar(METRIC_ACCURACY, accuracy, step=1)
+
+
 #%%
 
 # Window Cleanup
 cv2.destroyAllWindows()
 plt.close('all')
 #%%
-
-# from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization, Conv2D, MaxPool2D
 
 
 # setup datagenerator
@@ -93,83 +174,37 @@ dgen_val = image_generator.flow_from_dataframe(
 temp_batch = dgen_val.next()
 temp_img = temp_batch[0][0]
 
-# 5x5 kernels, stride 3:
-# 128 / 3 = ca 43 -> 43x43 img
-# 5*5 * 43*43 = 46225 multiplications per img
-# subpooling 3x3:
-# 43 / 3 = ca 15 -> 15x15 img
-# 15*15=225 flattened parameters
 
+#%%
+session_num = 0
+path_log = "D:\\ECM_PROJECT\\pythoncode\\V_02\\learning\\logs\\tuning"
 
-# https://www.tensorflow.org/tensorboard/hyperparameter_tuning_with_hparams
-HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([16, 32]))
-HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1, 0.5))
-HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
-
-METRIC_ACCURACY = 'accuracy'
-
-
-# setup model
-print("setup model")
-model = keras.models.Sequential()
-
-model.add(keras.layers.Conv2D(filters = 16, kernel_size = (3,3), strides=(1,1),
-                        input_shape=temp_img.shape, activation = 'relu'))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.MaxPool2D(pool_size=(2,2))) # 64*64
-model.add(keras.layers.Dropout(0.3))
-
-model.add(keras.layers.Conv2D(filters = 32, kernel_size = (3,3), activation = 'relu'))
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.MaxPool2D(pool_size=(2,2))) # 32*32
-model.add(keras.layers.Dropout(0.4))
-
-# model.add(layers.Conv2D(filters = 64, kernel_size = (3,3), activation = 'relu'))
-# model.add(tf.keras.layers.BatchNormalization())
-# model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2))) # 16*16
-# model.add(tf.keras.layers.Dropout(0.5))
-
-model.add(keras.layers.Flatten())
-units = 512 # num of hidden layers
-
-model.add(keras.layers.Dense(units = units, activation = 'relu')) # hidden layer 1
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Dropout(0.5))
-
-model.add(keras.layers.Dense(units = units, activation = 'relu')) # hidden layer 2
-model.add(keras.layers.BatchNormalization())
-model.add(keras.layers.Dropout(0.5))
-
-model.add(keras.layers.Dense(units=2, activation='sigmoid'))   # output layer
-
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-early_stop = keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 5)
-
-# start training
-print("start training")
-model.fit(dgen_train, 
-        epochs = 64, 
-        validation_data = dgen_val,
-        verbose=1, 
-        callbacks=early_stop )
-
-plt.close("all")
-metrics = pd.DataFrame(model.history.history)
-metrics[['loss','val_loss']].plot()
-plt.hlines(1,0,model.history.epoch[-1],linestyles="dashed")
-plt.title("Loss")
-metrics[['accuracy','val_accuracy']].plot()
-plt.hlines(1,0,model.history.epoch[-1],linestyles="dashed")
-plt.title("Accuracy")
-
-print(model.summary())
-# model.evaluate(x_test,y_test,verbose=1)
-
-# predictions = model.predict_classes(x_test)
+for num_units in HP_NUM_UNITS.domain.values:
+    for filters in HP_NUM_FILTERS.domain.values:
+        # for kernel_size in HP_KERNEL_SIZE.domain.values:
+            # for strides in HP_STRIDES.domain.values:
+        # for dropout in HP_DROPOUT.domain.values:
+        # for pool_size in HP_POOL_SIZE.domain.values:
+            # for optimizer in HP_OPTIMIZER.domain.values:
+        hparams = {
+            HP_NUM_UNITS: num_units,
+            HP_NUM_FILTERS: filters,
+            # HP_KERNEL_SIZE: kernel_size,
+            # HP_STRIDES: strides,
+            # HP_DROPOUT: dropout,
+            # HP_POOL_SIZE: pool_size,
+            # HP_OPTIMIZER: optimizer,
+        }
+        run_name = "run-{:012}".format(session_num)
+        print('\n--- Starting trial: %s' % run_name)
+        print({h.name: hparams[h] for h in hparams})
+        print()
+        path_file = os.path.join(path_log, run_name)
+        run(path_file, dgen_train,dgen_val,temp_img.shape,2,hparams,10)
+        session_num += 1
 
 
 
-    
     
     
     
