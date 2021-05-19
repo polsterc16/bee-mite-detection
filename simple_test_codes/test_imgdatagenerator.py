0c5d0a463c182d828700cc8eb176a0cbd32ec10d
chris <christoph.polster@edu.fh-joanneum.at>
2021-05-19 10:44:38 +0200

    added testfile to show the effects of the imgDataGen from keras

---
+++ simple_test_codes/test_imgdatagenerator.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 10:18:49 2021
+
+@author: Admin
+"""
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import os
+
+from tqdm import tqdm
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+
+#%% Setup
+
+f_dir = "imgdatagenerator_data"
+f_names = ["0.png", "1.png", "2.png", "3.png"]
+save_dir = "imgdatagenerator_data/output"
+
+data={"fnames":f_names,"categ":"1"}
+df=pd.DataFrame.from_dict(data)
+#%% Initiate ImageDataGenerator settings
+image_generator = ImageDataGenerator(
+                                    rotation_range=45,
+                                    width_shift_range=0.1,
+                                    height_shift_range=0.1,
+                                    zoom_range=.1,
+                                    )
+#%% Create object of this ImageDataGenerator for img files
+gen = image_generator.flow_from_dataframe(df, 
+                                          directory=f_dir,
+                                          x_col="fnames",
+                                          y_col="categ",
+                                          target_size=(64,64),
+                                          color_mode="grayscale",
+                                          save_to_dir=save_dir,
+                                          save_prefix="test_"
+                                          )
+#%% create output images 4 times for comparison
+for i in range(4):
+    ret=gen.next()
